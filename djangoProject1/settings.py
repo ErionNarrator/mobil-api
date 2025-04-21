e26eb1c7eced36297aa41709f080e2e560d8ed4f
ErionNarrator <nikitapavliy@gmail.com>
2025-04-21 23:35:28 +0300

    dbUP

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'BankAPI',
+        'USER': 'postgres',
+        'PASSWORD': 'Root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
