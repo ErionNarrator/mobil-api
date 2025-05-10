dcd7e97eacd3e4c061047d07d781d05b7c7c7be8
ErionNarrator <nikitapavliy@gmail.com>
2025-05-11 00:27:42 +0300

    UPdata

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'BankAPI',
+        'NAME':'Mobilapi',
         'USER': 'postgres',
         'PASSWORD': 'Root',
         'HOST': 'localhost',
